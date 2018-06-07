b31b8012eb31e1ac828f46ad7eeaed3c63e25eb3
PiotrH <piotr.holubb@gmail.com>
2018-06-07 21:43:22 +0200

    cards

--- scripts.js
+++ scripts.js
@@ -8,9 +8,27 @@ $( document ).ready(function() {
             url : apiURL + searchvalue + '&entity=song',
             dataType : 'jsonp',
             success: function(data){
-               $(data.results).each(function(index,value){
-                   console.log(value.artistName);
-               })
+               var res = data['results'];
+               for (var i=0; i<res.length; i++){
+                    
+                    var cards = `<div class="card">
+                                    <div class="logo_song">
+                                        <img src="images/logo_song.jpg">
+                                    </div>
+                                    <div class="title">
+                                    <div class="song">
+                                        <p>Castle on the Hill</p>
+                                    </div>
+                                    <div class="line">
+                                    </div>
+                                    <div class="author">
+                                        <p>By Ed Sheeran</p>
+                                    </div>
+                                </div>
+                                </div>`;
+                    var html = $.parseHTML(cards);
+                    $('#newcard').append(html);
+               }
             }
         });
     });
